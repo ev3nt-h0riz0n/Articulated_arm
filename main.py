62c1c4909bc37c0803968309295875f9a84d5bcc
Dorian Neumann <168371121+ev3nt-h0riz0n@users.noreply.github.com>
2025-06-01 20:52:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ from OpenGL.GLU import *
 from OpenGL.GL import *
 from Articulated_arm import *
 from Controls import *
+from Surroundings import *
+from Coordinates import *
 
 def udp_listener(rot_vars):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -31,34 +33,24 @@ def udp_listener(rot_vars):
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5006
 
-def light():
-        #Oświetlenie (żeby było widać, że 3D)
-    glEnable(GL_DEPTH_TEST)  # Potrzebne do poprawnego nakładania się obiektów
-
-    # Światło i cieniowanie
-    glEnable(GL_LIGHTING) #To włącza oświetlenie
-    glEnable(GL_LIGHT0) #To włącza pierwsze źródło światła
-    glEnable(GL_NORMALIZE)  # Automatyczne normalizowanie wektorów normalnych
-
-    # Ustawienie źródła światła
-    glLightfv(GL_LIGHT0, GL_POSITION,  (1, 1, -3, 0))  # Światło kierunkowe z góry z przodu
-    glLightfv(GL_LIGHT0, GL_DIFFUSE,   (1.0, 1.0, 1.0, 1.0))  # Białe światło
-    glLightfv(GL_LIGHT0, GL_SPECULAR,  (1.0, 1.0, 1.0, 1.0))
-
-    # Materiał - DO DOPRACOWANIA
-    glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, (0.5, 0.5, 0.5, 1.0))
-    glMaterialfv(GL_FRONT, GL_SPECULAR, (1.0, 1.0, 1.0, 1.0))
-    glMaterialf(GL_FRONT, GL_SHININESS, 50.0) 
-
-
-
 #Ta funkcja odpowiada za całą symulację :)
 def init():
     xrot, yrot, rot1, rot2, rot3 = 0,0,0,0,0
     rot_vars = [rot1, rot2, rot3]
     listener_thread = threading.Thread(target=udp_listener, args=(rot_vars,), daemon=True)
     listener_thread.start()
-    magnet_on = False  # Stan chwytaka magnetycznego
+
+
+    #Obsluga chwytaka magnesowego
+    magnet_on = False
+    usedsuccesfully = False
+    mr1,mr2,mr3=0,0,0 #Sprawdzenie w jakim obrocie znajduje sie robot po upuszczeniu przedmiotu
+
+    #Do obsługi przesunięcia robota w konkretne współrzędne
+    r1,r2,r3 = 0,0,0  #Sprawdzanie obecnego obrotu robota w momencie włączenia funkcji
+    movingworking = 0 #Czy przesuwanie jest w toku?
+
+
     #Inicjalizacja okna i ustawienie pozycji operatora
     pygame.init() 
     display = (800,600)
@@ -75,6 +67,10 @@ def init():
         for event in pygame.event.get():
             if event.type == QUIT:
                 running = False
+            elif event.type == KEYDOWN:
+                if event.key == K_m : 
+                    magnet_on = not magnet_on
+                    usedsuccesfully = True
 
         # Update from UDP listener
         rot1 = rot_vars[0]
@@ -82,21 +78,33 @@ def init():
         rot3 = rot_vars[2]
 
         xrot, yrot = keyscamera(xrot, yrot)
-        rot1 = Keys1(rot1)
-        rot2 = Keys2(rot2)
-        rot3 = Keys3(rot3)
+        if movingworking == 0:    
+            rot1 = Keys1(rot1)
+            rot2 = Keys2(rot2)
+            rot3 = Keys3(rot3)
+
+        #Funkcja odpowiadajaca za przemieszczenie do konkretnego punktu
+        result = GetToPosition() 
+        if result:
+            r1,r2,r3 = result
+            print(result)
+            movingworking=1
+        
+        if movingworking == 1:
+            rot1, rot2, rot3, movingworking = setcoordinates(r1,r2,r3,rot1,rot2,rot3)
+
+
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-        # Apply camera rotation to the whole scene (room and robot)
+        #Ruch kamery
         glPushMatrix()
         glRotatef(xrot, 1, 0, 0)
         glRotatef(yrot, 0, 1, 0)
 
-        draw_room(size=5)  # Draw the room
+        draw_room(size=5)  #Rysowanie pokoju
 
-        # Raise the robot above the floor (e.g., y = 0.2)
+        #Rysowanie robota
         glPushMatrix()
-        glTranslatef(0.0, 0.01, 0.0)  # Adjust this value as needed
         base()
 
         glPushMatrix()  # 1 Segment
@@ -117,14 +125,45 @@ def init():
         glTranslatef(0, 0.0, 0.65)
         EffectorJoint()
 
-        glPopMatrix()  # Koniec chwytaka
-        glPopMatrix()  # Koniec 3 segmentu
-        glPopMatrix()  # Koniec 2 segmentu
-        glPopMatrix()  # Koniec 1 segmentu
-        glPopMatrix()  # Koniec bazy
+        if magnet_on: #Prymitywne dzialanie magnesu
+            glTranslatef(0, -0.05, 0.19)
+            Rocket()
+            w1,w2,w3 = position(rot1,rot2,rot3)
+            mr1,mr2,mr3 = rot1,rot2,rot3
+        elif usedsuccesfully == 1:
+            glPopMatrix()
+            glPopMatrix()
+            glPopMatrix()
+            glPopMatrix()
+            glPopMatrix()
 
-        glPopMatrix()
+            #Rysowanie rakiety po upuszczeniu przez chwytak magnetyczny. Trzeba powtórzyć całą sekwencje
+            #przemieszczeń, żeby zwrócić tamte współrzędne.
+            glRotatef(mr1, 0, 1, 0)
+            glTranslatef(0.0, 0.7, 0)         
+            glRotatef(mr2, -1, 0, 0)
+            glTranslatef(0, 0, 0.7)          
+            glRotatef(mr3, 1, 0, 0)
+            glTranslatef(0, 0.0, 0.65)       
+            glTranslatef(0, -0.05, 0.19)    
+            Rocket()
 
+
+        if magnet_on or (usedsuccesfully==0 and magnet_on == 0):
+            glPopMatrix() #Koniec chwytaka
+            glPopMatrix() #Koniec 3 segmentu
+            glPopMatrix() #Koniec 2 segmentu
+            glPopMatrix() #Koniec 1 segmentu
+            glPopMatrix() #Koniec bazy
+
+            if magnet_on==0 and usedsuccesfully==0:
+                glTranslatef(0.2, 0.3, 1)
+                Rocket()
+
+        #Generacja osobnego obiektu (rakiety)
+
+        glPopMatrix() #Kamera
+        print(position(rot1, rot2, rot3))
 
         pygame.display.flip()
         if pygame.key.get_pressed()[pygame.K_ESCAPE]:
